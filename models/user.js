a5eef522e386695ea94beb0a6c0f62e9c9d681fe
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-04 16:44:08 +0530

    changes in schema

--- models/user.js
+++ models/user.js
@@ -1,11 +1,34 @@
 let mongoose = require('mongoose');
-
+let bcrypt = require('bcrypt');
 let uSchema = new mongoose.Schema({
 
-    name: { type: String },
+    name: { type: String, index: true },
     email: { type: String },
-    rollNo: { type: String }
+    rollNo: { type: String },
+    password: { type: String }
 }, { timestamps: true });
 
+uSchema.pre('save', function (next) {
 
-module.exports=mongoose.model('users',uSchema);
+    let user = this;
+    if (!user.isModified('password' && !user.password)) {
+        {
+            next();
+        }
+    }
+
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+
+
+uSchema.post('save', function (doc) {
+    console.log('doc id' + doc._id);
+});
+
+module.exports = mongoose.model('users', uSchema);
