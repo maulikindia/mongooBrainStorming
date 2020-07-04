14153ec059ad137b6b9b42b0ca0c9a4379d86606
maulikindia <60645304+maulikindia@users.noreply.github.com>
2020-07-04 16:42:41 +0530

    pre and post hooks in schema and bcryopt for password hashing

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ let config = require('../config/db');
 
 
 
-
+let bcrypt = require('bcrypt');
 router.post('/', async (req, res) => {
 
   let bodyData = req.body;
@@ -21,11 +21,12 @@ router.post('/', async (req, res) => {
     myObj.name = bodyData.name + '-' + '0' + i;
     myObj.rollNo = i;
     myObj.email = myObj.name + '@' + 'gmail.com';
+    myObj.password = myObj.rollNo + ' ' + myObj.email;
     userArray.push(myObj);
   }
 
 
-
+  // let newArr = await encryptPass(userArray);
   await user.insertMany(userArray, async (err, respo) => {
     if (err) {
       return res.json({ status: false, msg: err, data: [] });
@@ -37,9 +38,21 @@ router.post('/', async (req, res) => {
 
 });
 
+router.post('/add', async (req, res) => {
+  let bodyData = req.body;
+  await user.create(bodyData, async (err, respo) => {
+    if (err) {
+      return res.json({ status: false, msg: err, data: [] });
+    }
+    else if (respo !== null) {
+      return res.json({ status: true, msg: '', data: respo });
+    }
+  });
+
+})
+
 
 router.get('/', async (req, res) => {
-
   await user.find({}, async (err, respo) => {
     if (err) {
       return res.json({ status: false, msg: err, data: [] });
@@ -50,13 +63,12 @@ router.get('/', async (req, res) => {
 
       return res.json({ status: true, msg: '', data: respo });
     }
-  }).sort({ name: -1 });
+  }).sort({ name: 1 });
 
 })
 
 router.delete('/', async (req, res) => {
-
-  await user.updateMany({}, { $unset: '' }, async (err, respo) => {
+  await user.deleteMany({}, async (err, respo) => {
     if (err) {
       return res.json({ status: false, msg: err, data: [] });
     }
@@ -68,5 +80,26 @@ router.delete('/', async (req, res) => {
 
 
 });
+
+
+async function encryptPass(arr) {
+  for await (let mArr of arr) {
+    if (mArr.password !== null) {
+      await bcrypt.genSalt(10, async function (err, salt) {
+        if (err) {
+          return res.json({ status: false, msg: err, data: [] });
+        }
+        await bcrypt.hash(mArr.password, salt, function (err, hash) {
+          if (err) {
+            return res.json({ status: false, msg: err, data: [] });
+          }
+          let hashValue = hash;
+          mArr.password = null;
+        });
+      });
+    }
+  }
+  return arr;
+}
 
 module.exports = router;
